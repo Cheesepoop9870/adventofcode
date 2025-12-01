45266572de11adbb6abc95dad851a9254443a7c9
Cheesepoop9870 <wongkids571@gmail.com>
2025-12-01 14:14:03 -0500

    day 1

---
+++ day1.py
@@ -0,0 +1,36 @@
+dial = 50
+directions = input().split('\r')
+# directions.pop(len(directions)-1)
+directions_pm = []
+directions_amt = []
+direction_result_list = []
+direction_pass = 0
+for direction in directions:
+    directions_pm.append(direction[0])
+    directions_amt.append(int(direction[1:]))
+print(directions)
+print("")
+print(directions_pm)
+print(directions_amt)
+for x in range(0, len(directions)):
+    if directions_pm[x] == 'L':
+        direction_pass = direction_pass + (directions_amt[x]//100)
+        direction_temp = directions_amt[x] % 100
+        dial = dial - direction_temp
+        if dial < 0:
+            dial = dial+100
+        direction_result_list.append(dial)
+    
+    elif directions_pm[x] == 'R':34
+        direction_pass = direction_pass + (directions_amt[x]//100)
+        direction_temp = directions_amt[x] % 100
+        dial = dial + direction_temp
+        if dial > 99:
+            dial = dial-100
+        direction_result_list.append(dial)
+print("----------")
+print(direction_result_list)
+print("----------")
+print(direction_result_list.count(0))
+print(direction_result_list.count(0)+direction_pass)
+                
