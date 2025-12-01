7b9c4080b63952b68b20a67ccf41aab33ab79600
Cheesepoop9870 <wongkids571@gmail.com>
2025-12-01 14:34:56 -0500

    Refactor direction input handling and calculations

--- day1.py
+++ day1.py
@@ -1,10 +1,11 @@
 dial = 50
-directions = input().split('\r')
+directions = input("h:").split('\r')
 # directions.pop(len(directions)-1)
 directions_pm = []
 directions_amt = []
 direction_result_list = []
 direction_pass = 0
+direction_pass_list = []
 for direction in directions:
     directions_pm.append(direction[0])
     directions_amt.append(int(direction[1:]))
@@ -13,21 +14,25 @@ print("")
 print(directions_pm)
 print(directions_amt)
 for x in range(0, len(directions)):
+    direction_pass = 0
     if directions_pm[x] == 'L':
-        direction_pass = direction_pass + (directions_amt[x]//100)
+        direction_pass = directions_amt[x]//100
         direction_temp = directions_amt[x] % 100
         dial = dial - direction_temp
         if dial < 0:
             dial = dial+100
+            direction_pass = direction_pass + 1
         direction_result_list.append(dial)
-    
-    elif directions_pm[x] == 'R':34
-        direction_pass = direction_pass + (directions_amt[x]//100)
+        direction_pass_list.append(direction_pass)
+    elif directions_pm[x] == 'R':
+        direction_pass = directions_amt[x]//100
         direction_temp = directions_amt[x] % 100
         dial = dial + direction_temp
         if dial > 99:
             dial = dial-100
+            direction_pass += 1 
         direction_result_list.append(dial)
+        direction_pass_list.append(direction_pass)
 print("----------")
 print(direction_result_list)
 print("----------")
