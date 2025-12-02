ec88bb9d40e1cf59c854a7334dc04d1a58a25067
Cheesepoop9870 <wongkids571@gmail.com>
2025-12-02 17:21:14 -0500

    Implement range matching and summation of patterns
    Added functionality to find and sum repeated number patterns within specified ranges.

--- day2-1.py
+++ day2-1.py
@@ -3,5 +3,16 @@ rangelist = input("h:").split(",")
 # max length of invalid is 6, and all invalid ids have an even length
 for x in range(0, len(rangelist)): #turns list into a 2d list
     rangelist[x] = rangelist[x].split("-")
-rangelist = [[int(y[0]), int(y[1])] for y in rangelist]
+rangelist = [[int(y[0]), int(y[1])] for y in rangelist] #turns all ranges upper and lower bounds into ints
 print(rangelist)
+rangematch = []
+for idrange in rangelist: 
+    for x in range(idrange[0], idrange[1]):
+        # print(x)
+        if len(str(x)) % 2 == 0: #check if the length is an even amount so it dosent take forever
+                match = re.search(rf"(.{{{len(str(x)) // 2 }}})\1", str(x)) #regex to match repeated strings that is half the length of of the actual string
+                if match:
+                    rangematch.append(int(match.group())) #add the match to a group
+print("------")
+print(rangematch)
+print(sum(rangematch))
