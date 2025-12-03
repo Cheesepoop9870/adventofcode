59ccf79ec0ff0fbb9f1c78fc9202ad60c72fc81e
Cheesepoop9870 <wongkids571@gmail.com>
2025-12-02 19:35:55 -0500

    Fix range iteration and deduplicate rangematch
    Adjust range iteration and matching logic to include upper bound and avoid duplicates.

--- day2-2.py
+++ day2-2.py
@@ -15,14 +15,15 @@ rangelist = [[int(y[0]), int(y[1])] for y in rangelist] #turns all ranges upper 
 print(rangelist)
 rangematch = []
 for idrange in rangelist: 
-    for i in range(idrange[0], idrange[1]):
-        # print(x) # it was too much lol
+    for i in range(idrange[0], idrange[1]+1):
+        # print(i) # it was too much lol
         lenfactor = getfactors(len(str(i)))
-        for ii in range(1, len(str(i))): #might add +1
+        for factor in lenfactor:#ii in range(1, len(str(i))+1): #might add +1
             for factor2 in lenfactor:
-                match = re.search(rf"(.{{{factor2}}})\1{{{ii}}}", str(i)) 
-                if match:
+                match = re.search(rf"(.{{{factor2}}})\1{{{factor-(1 if factor != 1 else 0)}}}", str(i)) 
+                if match and (int(match.group()) >= idrange[0] and int(match.group()) <= idrange[1]):
                     rangematch.append(int(match.group())) #add the match to a group
+    # print(idrange)            
 print("------")
 print(rangematch)
-print(sum(rangematch))
+print(sum(list(set(rangematch))))
